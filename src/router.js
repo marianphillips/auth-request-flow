9f5ec057073b5b5aabe3629917cceb2754c2c54d
marianphillips <79763160+marianphillips@users.noreply.github.com>
2022-04-11 17:58:22 +0100

    updated

--- src/router.js
+++ src/router.js
@@ -14,10 +14,25 @@ const mockUser = {
 };
 
 router.post('/login', (req, res) => {
+    if(req.body.username && req.body.password && req.body.username === mockUser.username && req.body.password === mockUser.password) {
+        res.json(jwt.sign({username: mockUser.username}, "secret"))
+       }
+    else {
+        res.json({error: "invalid username or password"}) 
+    }
 
+    
 });
 
 router.get('/profile', (req, res) => {
+
+    try {
+        jwt.verify(req.rawHeaders[5].slice(8, req.rawHeaders[5].length - 1), "secret")
+        return res.json(mockUser)
+      } catch(err) {
+        if(err) return res.json("Error")
+      }
+
 
 });
 
